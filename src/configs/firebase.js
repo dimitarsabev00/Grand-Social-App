91572df9445ee06fb9daa2d0c1a12d061ab2c31d
Dimitar <wopz1337@gmail.com>
2022-10-26 21:36:12 +0300

    Added Firebase Config

---
+++ src/configs/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBpPPBfDQxL87WfIuZNWfb4U2P-OLnvB9Q",
+  authDomain: "linkedin-clone-516d5.firebaseapp.com",
+  projectId: "linkedin-clone-516d5",
+  storageBucket: "linkedin-clone-516d5.appspot.com",
+  messagingSenderId: "368523219379",
+  appId: "1:368523219379:web:9c5763a357412cb4cafbc6",
+  measurementId: "G-LB833Y2XEQ",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
