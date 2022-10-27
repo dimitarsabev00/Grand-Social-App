a3db684e10d8ac200a5380d6af9011442a71c0c2
Dimitar <wopz1337@gmail.com>
2022-10-27 18:50:27 +0300

    Added Header + Styling

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import HeaderOption from "../HeaderOption/HeaderOption";
+import HomeIcon from "@mui/icons-material/Home";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+
+import ChatIcon from "@mui/icons-material/Chat";
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header_left">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/3536/3536505.png"
+          alt=""
+        />
+        <div className="header_search">
+          <SearchIcon />
+          <input type="text" placeholder="Search.." />
+        </div>
+      </div>
+      <div className="header_right">
+        <HeaderOption title="Home" Icon={HomeIcon} />
+        <HeaderOption title="My Network" Icon={SupervisorAccountIcon} />
+        <HeaderOption title="Jobs" Icon={BusinessCenterIcon} />
+        <HeaderOption title="Messaging" Icon={ChatIcon} />
+        <HeaderOption title="Notification" Icon={NotificationsIcon} />
+        <HeaderOption
+          avatar="http://s3.amazonaws.com/37assets/svn/765-default-avatar.png"
+          title="Me"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
