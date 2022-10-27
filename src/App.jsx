d8db25b732dd23d08d643afdf9976b5468516cd2
Dimitar <wopz1337@gmail.com>
2022-10-27 18:50:46 +0300

    Import Header In App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import "./App.css";
-
+import Header from "./components/Header/Header";
 function App() {
   return (
     <div className="App">
-      <h1>Lets Build Linkedln Clone</h1>
-
-      {/* {Header} */}
+      <Header />
       {/* {AppBody} */}
       {/* {SideBar} */}
       {/* {Feed} */}
