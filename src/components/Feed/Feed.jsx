0b23e1293ccabe4b4bbe4147c2403432bc3d9a56
Dimitar <wopz1337@gmail.com>
2022-10-27 22:32:49 +0300

    Added Start A Post Container

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,7 +1,35 @@
 import React from "react";
-
+import CreateIcon from "@mui/icons-material/Create";
+import "./Feed.css";
+import InputOption from "../InputOption/InputOption";
+import ImageIcon from "@mui/icons-material/Image";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import EventNoteIcon from "@mui/icons-material/EventNote";
+import CalendarViewDayIcon from "@mui/icons-material/CalendarViewDay";
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    <div className="feed">
+      <div className="feed_inputContainer">
+        <div className="feed_input">
+          <CreateIcon />
+          <form>
+            <input type="text" placeholder="Start a post" />
+            <button type="submit">Send</button>
+          </form>
+        </div>
+        <div className="feed_inputOptions">
+          <InputOption Icon={ImageIcon} title="Photo" color="#70b5f9" />
+          <InputOption Icon={SubscriptionsIcon} title="Video" color="#e7a33e" />
+          <InputOption Icon={EventNoteIcon} title="Event" color="#c0cbcd" />
+          <InputOption
+            Icon={CalendarViewDayIcon}
+            title="Write article"
+            color="#7fc15e"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Feed;
