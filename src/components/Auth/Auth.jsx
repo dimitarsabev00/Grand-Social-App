c12b590866057a844d6db653fec5e905aa781aa4
Dimitar <wopz1337@gmail.com>
2022-10-29 22:31:41 +0300

    Added Auth Component & Added Register Functionality + Dispatch User Object To userSlice

---
+++ src/components/Auth/Auth.jsx
@@ -0,0 +1,65 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../../app/features/userSlice";
+import { auth } from "../../configs/firebase";
+import "./Auth.css";
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  
+  const register = () => {
+    if (!email || !password) {
+      alert("Please enter your email & password");
+    }
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userAuth) => {
+        dispatch(
+          login({
+            email: userAuth.user.email,
+            uid: userAuth.user.uid,
+          })
+        );
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+  return (
+    <div className="auth">
+      <form>
+        <input
+          type="email"
+          name=""
+          id=""
+          placeholder="Email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <input
+          type="password"
+          name=""
+          id=""
+          placeholder="Password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button type="submit">Sign In</button>
+      </form>
+      <p>
+        Not a member?{" "}
+        <span className="auth_register" onClick={register}>
+          Register Now
+        </span>
+      </p>
+    </div>
+  );
+};
+
+export default Auth;
