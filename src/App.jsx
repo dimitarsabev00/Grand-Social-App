6e49c63ae3df9951d4b2b60f07af3a82c2320f61
Dimitar <wopz1337@gmail.com>
2022-10-29 22:27:22 +0300

    Added Auth Conditional Rendering In App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./App.css";
+import { selectUser } from "./app/features/userSlice";
+import Auth from "./components/Auth/Auth";
 import Feed from "./components/Feed/Feed";
 import Header from "./components/Header/Header";
 import SideBar from "./components/SideBar/SideBar";
 function App() {
+  const user = useSelector(selectUser);
   return (
     <div className="app">
       <Header />
-      <div className="app_body">
-        <SideBar />
-        <Feed />
-        {/* Widgets */}
-      </div>
+      {!user ? (
+        <Auth />
+      ) : (
+        <div className="app_body">
+          <SideBar />
+          <Feed />
+          {/* Widgets */}
+        </div>
+      )}
     </div>
   );
 }
