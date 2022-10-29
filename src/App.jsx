94342bccad569486cdd3b3e5311c4f2da2fb6b3e
Dimitar <wopz1337@gmail.com>
2022-10-29 22:44:59 +0300

    Added Persisting Current User

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
-import { selectUser } from "./app/features/userSlice";
+import { login, logout, selectUser } from "./app/features/userSlice";
 import Auth from "./components/Auth/Auth";
 import Feed from "./components/Feed/Feed";
 import Header from "./components/Header/Header";
 import SideBar from "./components/SideBar/SideBar";
+import { auth } from "./configs/firebase";
 function App() {
   const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (userAuth) => {
+      if (userAuth) {
+        dispatch(
+          login({
+            email: userAuth.email,
+            uid: userAuth.uid,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, []);
+
   return (
     <div className="app">
       <Header />
