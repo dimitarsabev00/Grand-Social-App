caa4297693e1cc51319528c8003de3396e4a1c12
Dimitar <wopz1337@gmail.com>
2022-10-31 11:26:11 +0200

    Added Routing In App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,14 @@
-import { onAuthStateChanged } from "firebase/auth";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
-import { login, logout, selectUser } from "./app/features/userSlice";
-import Auth from "./components/Auth/Auth";
-import Feed from "./components/Feed/Feed";
-import Header from "./components/Header/Header";
-import SideBar from "./components/SideBar/SideBar";
-import { auth } from "./configs/firebase";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 function App() {
-  const user = useSelector(selectUser);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    onAuthStateChanged(auth, (userAuth) => {
-      if (userAuth) {
-        dispatch(
-          login({
-            email: userAuth.email,
-            uid: userAuth.uid,
-          })
-        );
-      } else {
-        dispatch(logout());
-      }
-    });
-  }, []);
-
   return (
-    <div className="app">
-      <Header />
-      {!user ? (
-        <Auth />
-      ) : (
-        <div className="app_body">
-          <SideBar />
-          <Feed />
-          {/* Widgets */}
-        </div>
-      )}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
