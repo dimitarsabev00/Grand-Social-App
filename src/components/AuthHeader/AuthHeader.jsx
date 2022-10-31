44baf2ae2a69dbdc755b7c3e4d40ed274dac2dfa
Dimitar <wopz1337@gmail.com>
2022-10-31 11:31:16 +0200

    Create AuthHeader Component

---
+++ src/components/AuthHeader/AuthHeader.jsx
@@ -0,0 +1,17 @@
+import "./AuthHeader.css";
+const AuthHeader = () => {
+  return (
+    <div className="page_wrapper">
+      <nav className="header_nav_wrapper">
+        <a href="/">
+          <img
+            src="https://media.istockphoto.com/vectors/camera-icon-simple-style-isolated-vector-illustration-on-white-vector-id1278996256?k=20&m=1278996256&s=612x612&w=0&h=bTKSsWlqGPZKZL4b-JCwU825aHySeU88-ZNFm8LFOsc="
+            alt=""
+          />
+        </a>
+      </nav>
+    </div>
+  );
+};
+
+export default AuthHeader;
