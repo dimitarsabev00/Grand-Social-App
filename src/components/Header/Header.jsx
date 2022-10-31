6dc2e14c78e412ddcee5633f4110e1074607cbfc
Dimitar <wopz1337@gmail.com>
2022-10-31 11:30:22 +0200

    Change Header Logo & Add Classes For Each Html Elements

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ import HomeIcon from "@mui/icons-material/Home";
 import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
 import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
 import NotificationsIcon from "@mui/icons-material/Notifications";
-
+import LogoutIcon from "@mui/icons-material/Logout";
 import ChatIcon from "@mui/icons-material/Chat";
 import { useDispatch } from "react-redux";
 import { logout } from "../../app/features/userSlice";
@@ -18,24 +18,25 @@ const Header = () => {
     dispatch(logout());
     signOut(auth);
   };
+
   return (
-    <div className="header">
-      <div className="header_left">
+    <div className="container">
+      <div className="main_content">
         <img
-          src="https://cdn-icons-png.flaticon.com/512/3536/3536505.png"
+          src="https://media.istockphoto.com/vectors/camera-icon-simple-style-isolated-vector-illustration-on-white-vector-id1278996256?k=20&m=1278996256&s=612x612&w=0&h=bTKSsWlqGPZKZL4b-JCwU825aHySeU88-ZNFm8LFOsc="
           alt=""
         />
-        <div className="header_search">
+        <div className="search">
           <SearchIcon />
           <input type="text" placeholder="Search.." />
         </div>
       </div>
-      <div className="header_right">
+      <div className="nav">
         <HeaderOption title="Home" Icon={HomeIcon} />
         <HeaderOption title="My Network" Icon={SupervisorAccountIcon} />
         <HeaderOption title="Jobs" Icon={BusinessCenterIcon} />
         <HeaderOption title="Messaging" Icon={ChatIcon} />
-        <HeaderOption title="Notification" Icon={NotificationsIcon} />
+        <HeaderOption title="Notifications" Icon={NotificationsIcon} />
         <HeaderOption
           avatar="http://s3.amazonaws.com/37assets/svn/765-default-avatar.png"
           title="Me"
