66a0a44ad91ac8d1c836e27411f1a11cec6602ac
Dimitar <wopz1337@gmail.com>
2022-11-01 18:52:59 +0200

    Added Routing + Protected Route

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,29 @@ import "./App.css";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import { lazy, Suspense } from "react";
+import Protected from "./components/Protected";
+const SignUp = lazy(() => import("./pages/SignUp"));
+const Login = lazy(() => import("./pages/Login"));
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Suspense fallback={<p>Loading..</p>}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Protected>
+                <Home />
+              </Protected>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          {/* <Route path="/profile/:username" element={<ProfilePage />} /> */}
+          <Route path="*" element={<h1>Page Not Found</h1>} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
