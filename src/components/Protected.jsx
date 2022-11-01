9150be54d70f65c3f4ef6a08ab8fb7f8fc144715
Dimitar <wopz1337@gmail.com>
2022-11-01 18:52:09 +0200

    Added Protected Component

---
+++ src/components/Protected.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import { selectUser } from "../app/features/userSlice";
+const Protected = ({ children }) => {
+  const user = useSelector(selectUser);
+
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+  return children;
+};
+
+export default Protected;
