5de58911dc459b0f310aa73fff4fba246ee3b229
Dimitar <wopz1337@gmail.com>
2022-11-03 11:14:52 +0200

    Change Header Design

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,52 +1,104 @@
-import React from "react";
-import "./Header.css";
-import SearchIcon from "@mui/icons-material/Search";
-import HeaderOption from "../HeaderOption/HeaderOption";
-import HomeIcon from "@mui/icons-material/Home";
-import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
-import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
-import NotificationsIcon from "@mui/icons-material/Notifications";
-import LogoutIcon from "@mui/icons-material/Logout";
-import ChatIcon from "@mui/icons-material/Chat";
-import { useDispatch } from "react-redux";
-import { logout } from "../../app/features/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { logout, selectUser } from "../../app/features/userSlice";
 import { signOut } from "firebase/auth";
 import { auth } from "../../configs/firebase";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 const Header = () => {
-  const navigate = useNavigate();
+  const user = useSelector(selectUser);
+
   const dispatch = useDispatch();
   const logoutOfApp = () => {
     dispatch(logout());
     signOut(auth);
-    navigate("/login");
   };
 
   return (
-    <div className="container">
-      <div className="main_content">
-        <img
-          src="https://media.istockphoto.com/vectors/camera-icon-simple-style-isolated-vector-illustration-on-white-vector-id1278996256?k=20&m=1278996256&s=612x612&w=0&h=bTKSsWlqGPZKZL4b-JCwU825aHySeU88-ZNFm8LFOsc="
-          alt=""
-        />
-        <div className="search">
-          <SearchIcon />
-          <input type="text" placeholder="Search.." />
+    <header className="h-16 bg-white border-b border-gray-primary mb-8">
+      <div className="container mx-auto max-w-screen-lg h-full">
+        <div className="flex justify-between h-full">
+          <div className="text-gray-700 text-center flex items-center align-items cursor-pointer">
+            <h1 className="flex justify-center w-full">
+              <Link to="/" aria-label="Social Media Logo">
+                <img
+                  src="https://media.istockphoto.com/vectors/camera-icon-simple-style-isolated-vector-illustration-on-white-vector-id1278996256?k=20&m=1278996256&s=612x612&w=0&h=bTKSsWlqGPZKZL4b-JCwU825aHySeU88-ZNFm8LFOsc="
+                  alt=""
+                  className="mt-2 w-1/12"
+                />
+              </Link>
+            </h1>
+          </div>
+          <div className="text-gray-700 text-center flex items-center align-items">
+            {user ? (
+              <>
+                <Link to="/" aria-label="HomePage">
+                  <svg
+                    className="w-8 mr-6 text-black-light cursor-pointer"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                    />
+                  </svg>
+                </Link>
+                <button type="button" title="Sign Out" onClick={logoutOfApp}>
+                  <svg
+                    className="w-8 mr-6 text-black-light cursor-pointer"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
+                    />
+                  </svg>
+                </button>
+                <div className="flex items-center cursor-pointer">
+                  <Link to={`/profile/${auth.currentUser.displayName}`}>
+                    <img
+                      src="http://s3.amazonaws.com/37assets/svn/765-default-avatar.png"
+                      className="rounded-full h-8 w-8 flex"
+                      alt={`${auth.currentUser.displayName} profile`}
+                    />
+                  </Link>
+                </div>
+              </>
+            ) : (
+              <>
+                <Link to="/login">
+                  <button
+                    type="button"
+                    className="bg-blue-medium font-bold text-sm rounded text-white w-20 h-8"
+                  >
+                    {" "}
+                    Log In
+                  </button>
+                </Link>
+                <Link to="/sign-up">
+                  <button
+                    type="button"
+                    className="font-bold text-sm rounded text-blue-medium w-20 h-8"
+                  >
+                    Sign Up
+                  </button>
+                </Link>
+              </>
+            )}
+          </div>
         </div>
       </div>
-      <div className="nav">
-        <HeaderOption title="Home" Icon={HomeIcon} />
-        <HeaderOption title="My Network" Icon={SupervisorAccountIcon} />
-        <HeaderOption title="Jobs" Icon={BusinessCenterIcon} />
-        <HeaderOption title="Messaging" Icon={ChatIcon} />
-        <HeaderOption title="Notifications" Icon={NotificationsIcon} />
-        <HeaderOption
-          avatar="http://s3.amazonaws.com/37assets/svn/765-default-avatar.png"
-          title="Me"
-          OnClick={logoutOfApp}
-        />
-      </div>
-    </div>
+    </header>
   );
 };
 
