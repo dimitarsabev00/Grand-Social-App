c6105a7bf5ed258436d4251b965c5a5897ecf673
Dimitar <wopz1337@gmail.com>
2022-11-03 11:17:49 +0200

    Remove Protected Route From Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
-import Protected from "./components/Protected";
 const Home = lazy(() => import("./pages/Home"));
 const SignUp = lazy(() => import("./pages/SignUp"));
 const Login = lazy(() => import("./pages/Login"));
@@ -12,14 +11,7 @@ function App() {
     <Router>
       <Suspense fallback={<p>Loading..</p>}>
         <Routes>
-          <Route
-            path="/"
-            element={
-              <Protected>
-                <Home />
-              </Protected>
-            }
-          />
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/sign-up" element={<SignUp />} />
           {/* <Route path="/profile/:username" element={<ProfilePage />} /> */}
