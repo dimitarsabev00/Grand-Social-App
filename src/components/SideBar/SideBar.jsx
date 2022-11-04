b1cbbceb53cb0df99bd24e45ff6f9132b7d33d35
Dimitar <wopz1337@gmail.com>
2022-11-04 02:32:01 +0200

    Added SideBar Component With User (Current User) Component & Suggestions Users Component

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,49 +1,21 @@
-import { Avatar } from "@mui/material";
-import React from "react";
-import "./SideBar.css";
+import { memo } from "react";
+import useUser from "../../hooks/useUser";
+import Suggestions from "./Suggestions";
+import User from "./User";
 const SideBar = () => {
-  const recentItem = (topic) => {
-    return (
-      <div className="sidebar_recentItem">
-        <span className="sidebar_hash">#</span>
-        <p>{topic}</p>
-      </div>
-    );
-  };
+  const {
+    user: { docId, fullName, username, userId, following },
+  } = useUser();
   return (
-    <div className="sidebar">
-      <div className="sidebar_top">
-        <img
-          src="https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-          alt=""
-        />
-        <Avatar
-          src="http://s3.amazonaws.com/37assets/svn/765-default-avatar.png"
-          className="sidebar_avatar"
-        />
-        <h2>Dimitar Sabev</h2>
-        <h4>Junior React Developer</h4>
-        <h4>dimitar@abv.bg</h4>
-      </div>
-      <div className="sidebar_stats">
-        <div className="sidebar_stat">
-          <p className="sidebar_statText">Who's viewed you</p>
-          <p className="sidebar_statNumber">2,543</p>
-        </div>
-        <div className="sidebar_stat">
-          <p className="sidebar_statText">Views on post</p>
-          <p className="sidebar_statNumber">5,754</p>
-        </div>
-      </div>
-      <div className="sidebar_bottom">
-        <p>Recent</p>
-        {recentItem("reactjs")}
-        {recentItem("programming")}
-        {recentItem("javascript")}
-        {recentItem("nodejs")}
-      </div>
+    <div className="p-4">
+      <User username={username} fullName={fullName} />
+      <Suggestions
+        userId={userId}
+        following={following}
+        loggedInUserDocId={docId}
+      />
     </div>
   );
 };
 
-export default SideBar;
+export default memo(SideBar);
