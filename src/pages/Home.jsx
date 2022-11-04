7fcb69095e0f639fb2416fb04b1740986085bc8b
Dimitar <wopz1337@gmail.com>
2022-11-04 02:32:57 +0200

    Change Template For Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,11 +28,9 @@ const Home = () => {
     <div className="bg-gray-background">
       <Header />
 
-      <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
-        <p>Im Feed</p>
-        <p>Im SideBar</p>
-        {/* <Feed /> */}
-        {/* <SideBar /> */}
+      <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg ">
+        <Feed />
+        <SideBar />
       </div>
     </div>
   );
