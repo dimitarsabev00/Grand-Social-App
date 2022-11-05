329d2ffc153b0c7560f3f9c31037a4d973c128bb
Dimitar <wopz1337@gmail.com>
2022-11-05 02:47:11 +0200

    Create ImagePost Component

---
+++ src/components/Post/ImagePost.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ImagePost = () => {
+  return (
+    <img
+      src="https://celebrityaccess.com/wp-content/uploads/2018/10/instagram.jpg"
+      alt=""
+    />
+  );
+};
+
+export default ImagePost;
