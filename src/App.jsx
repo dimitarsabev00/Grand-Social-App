2f84ae95d68391aa7bd81c542f0a0701e91a3bf9
Dimitar <wopz1337@gmail.com>
2022-11-05 02:45:40 +0200

    Use This Hook In App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "./App.css";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
+import useAuthListener from "./hooks/useAuthListener";
 const Home = lazy(() => import("./pages/Home"));
 const SignUp = lazy(() => import("./pages/SignUp"));
 const Login = lazy(() => import("./pages/Login"));
 const ErrorPage = lazy(() => import("./pages/ErrorPage"));
 function App() {
+  const { user } = useAuthListener();
   return (
     <Router>
       <Suspense fallback={<p>Loading..</p>}>
