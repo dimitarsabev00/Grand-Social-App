5cab865ca2462983f71d56e6e3e5c7426c41c805
Dimitar <wopz1337@gmail.com>
2022-11-08 16:58:21 +0200

    Update HeaderUserProfile Component

--- src/components/Profile/HeaderUserProfile.jsx
+++ src/components/Profile/HeaderUserProfile.jsx
@@ -1,9 +1,97 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
-const HeaderUserProfile = () => {
+import useUser from "../../hooks/useUser";
+import { isUserFollowingProfile } from "../../services/firebase";
+const HeaderUserProfile = ({
+  postsCount,
+  profile: {
+    docId: profileDocId,
+    userId: profileUserId,
+    fullName,
+    followers = [],
+    following = [],
+    username: profileUsername,
+  },
+  followerCount,
+  setFollowerCount,
+  loggedInUsername,
+}) => {
+  const { user } = useUser();
   const [isFollowingProfile, setIsFollowingProfile] = useState(false);
-  return <div>HeaderUserProfile</div>;
+  const activeBtnFollow = user.username && user.username != profileUsername;
+  useEffect(() => {
+    const isLoggedInUserFollowingProfile = async () => {
+      const isFollowing = await isUserFollowingProfile(
+        user.username,
+        profileUserId
+      );
+      setIsFollowingProfile(!!isFollowing);
+    };
+    if (user.username && profileUserId) {
+      isLoggedInUserFollowingProfile();
+    }
+  }, [user.username, profileUserId]);
+
+  const handleToggleFollow = () => {
+    setIsFollowingProfile((isFollowingProfile) => !isFollowingProfile);
+    setFollowerCount({
+      followerCount: isFollowingProfile
+        ? followers.length - 1
+        : followers.length + 1,
+    });
+  };
+  return (
+    <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
+      <div className="container flex justify-center">
+        {user.username && (
+          <img
+            src="http://s3.amazonaws.com/37assets/svn/765-default-avatar.png"
+            alt={`${profileUsername} profile picture`}
+            className="rounded-full h-40 w-40 flex"
+          />
+        )}
+      </div>
+      <div className="flex items-center justify-center flex-col col-span-2">
+        <div className="container flex items-center">
+          <p className="text-2x1 mr-4">{profileUsername}</p>
+          {activeBtnFollow && (
+            <button
+              className="bg-blue-medium font-bold text-sm rounded text-white w-20 h-8"
+              type="button"
+              onClick={handleToggleFollow}
+            >
+              {isFollowingProfile ? "Unfollow" : "Follow"}
+            </button>
+          )}
+        </div>
+        <div className="container flex mt-4">
+          {!followers || !following ? (
+            <Skeleton count={1} width={677} height={24} />
+          ) : (
+            <>
+              <p className="mr-10">
+                <span className="font-bold">{postsCount}</span> posts
+              </p>
+              <p className="mr-10">
+                <span className="font-bold">{followers?.length}</span>
+                {` `}
+                {followers.length === 1 ? `follower` : `followers`}
+              </p>
+              <p className="mr-10">
+                <span className="font-bold">{following?.length}</span> following
+              </p>
+            </>
+          )}
+        </div>
+        <div className="container mt-4">
+          <p className="font-medium">
+            {!fullName ? <Skeleton count={1} height={24} /> : fullName}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HeaderUserProfile;
