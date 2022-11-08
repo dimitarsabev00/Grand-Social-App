fc64ecada68fe068038c4e743766008c771e6116
Dimitar <wopz1337@gmail.com>
2022-11-08 17:02:20 +0200

    Added getProfileInfoAndPosts Function + useReducer For State & Import HeaderUserProfile Component

--- src/components/Profile/UserProfile.jsx
+++ src/components/Profile/UserProfile.jsx
@@ -1,15 +1,40 @@
-import { useEffect } from "react";
+import { useReducer, useEffect } from "react";
+import { getUserPostsByUsername } from "../../services/firebase";
 import HeaderUserProfile from "./HeaderUserProfile";
 
-const UserProfile = ({ username }) => {
-  useEffect(() => {
-    const getProfileInfoAndPhotos = async () => {};
-    getProfileInfoAndPhotos();
-  }, []);
+const UserProfile = ({ user }) => {
+  const reducer = (state, newState) => ({ ...state, ...newState });
+  const initialState = {
+    profile: {},
+    postsCollection: [],
+    followerCount: 0,
+  };
 
+  const [{ profile, postsCollection, followerCount }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+  useEffect(() => {
+    const getProfileInfoAndPosts = async () => {
+      const posts = await getUserPostsByUsername(user.username);
+      dispatch({
+        profile: user,
+        postsCollection: posts,
+        followerCount: user.followers.length,
+      });
+    };
+    if (user.username) {
+      getProfileInfoAndPosts();
+    }
+  }, [user.username]);
   return (
     <div>
-      <HeaderUserProfile />
+      <HeaderUserProfile
+        postsCount={postsCollection ? postsCollection.length : 0}
+        profile={profile}
+        followerCount={followerCount}
+        setFollowerCount={dispatch}
+      />
     </div>
   );
 };
