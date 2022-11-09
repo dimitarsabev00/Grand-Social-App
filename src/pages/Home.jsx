91c12ab0edb8d871e42c140a641a23f611f1ed1c
Dimitar <wopz1337@gmail.com>
2022-11-09 13:05:44 +0200

    Delete Commented Code & Format All Code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,11 @@
-import { onAuthStateChanged } from "firebase/auth";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { auth } from "../configs/firebase";
-import { login, logout } from "../app/features/userSlice";
 import Feed from "../components/Feed/Feed";
 import Header from "../components/Header/Header";
 import SideBar from "../components/SideBar/SideBar";
 const Home = () => {
-  const dispatch = useDispatch();
-
   useEffect(() => {
     document.title = "Social Media App";
-    // onAuthStateChanged(auth, (userAuth) => {
-    //   if (userAuth) {
-    //     dispatch(
-    //       login({
-    //         email: userAuth.email,
-    //         uid: userAuth.uid,
-    //       })
-    //     );
-    //   } else {
-    //     dispatch(logout());
-    //   }
-    // });
   }, []);
   return (
     <div className="bg-gray-background">
@@ -30,6 +13,7 @@ const Home = () => {
 
       <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg ">
         <Feed />
+
         <SideBar />
       </div>
     </div>
