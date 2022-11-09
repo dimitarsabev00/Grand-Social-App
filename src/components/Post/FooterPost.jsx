32b34a25f1d57b9edc94125b68ca490c361e5a24
Dimitar <wopz1337@gmail.com>
2022-11-09 11:15:04 +0200

    Added Style For Post Description

--- src/components/Post/FooterPost.jsx
+++ src/components/Post/FooterPost.jsx
@@ -4,7 +4,7 @@ const FooterPost = ({ description, authorUsername }) => {
   return (
     <div className="p-4 pt-2 pb-0">
       <span className="mr-1 font-bold">{authorUsername}</span>
-      <span>{description}</span>
+      <span className="italic">{description}</span>
     </div>
   );
 };
