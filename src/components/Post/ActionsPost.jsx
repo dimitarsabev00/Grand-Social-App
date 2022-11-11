9d12133926d36a10f78540c9db8f97ef8acbc71f
Dimitar <wopz1337@gmail.com>
2022-11-11 11:44:09 +0200

    Added Share Icon

--- src/components/Post/ActionsPost.jsx
+++ src/components/Post/ActionsPost.jsx
@@ -49,12 +49,27 @@ const ActionsPost = ({ docId, totalLikes, likedPhoto, handleFocus }) => {
             stroke-width="1.5"
             stroke="currentColor"
             class="w-6 h-6"
-            className="w-6 text-black-light select-none cursor-pointer"
+            className="w-6 mr-4 text-black-light select-none cursor-pointer"
           >
             <path
               stroke-linecap="round"
               stroke-linejoin="round"
               d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
+            />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+            className="w-6 text-black-light select-none cursor-pointer"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z"
             />
           </svg>
         </div>
