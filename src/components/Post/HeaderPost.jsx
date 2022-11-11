a81332328e9c80435eef86ed27ad4bf051bb0a05
Dimitar <wopz1337@gmail.com>
2022-11-11 10:21:43 +0200

    Added Icon For Post Option

--- src/components/Post/HeaderPost.jsx
+++ src/components/Post/HeaderPost.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const HeaderPost = ({ authorUsername }) => {
   return (
-    <div className="flex border-b border-gray-primary h-4 p-4 py-8">
+    <div className="flex justify-between items-center border-b border-gray-primary h-4 p-4 py-8">
       <div className="flex items-center">
         <Link to={`/profile/${authorUsername}`} className="flex items-center">
           <img
@@ -14,6 +14,20 @@ const HeaderPost = ({ authorUsername }) => {
         </Link>
         <p className="font-bold">{authorUsername}</p>
       </div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-6 h-6"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
+        />
+      </svg>
     </div>
   );
 };
