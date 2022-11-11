8ee597d9cc21de724551d1f40abcb0d02759a451
Dimitar <wopz1337@gmail.com>
2022-11-11 11:47:45 +0200

    Change  like/likes To Like/Likes

--- src/components/Post/ActionsPost.jsx
+++ src/components/Post/ActionsPost.jsx
@@ -76,7 +76,7 @@ const ActionsPost = ({ docId, totalLikes, likedPhoto, handleFocus }) => {
       </div>
       <div className="p-4 py-0">
         <p className="font-bold">
-          {likes === 1 ? `${likes} like` : `${likes} likes`}
+          {likes === 1 ? `${likes} Like` : `${likes} Likes`}
         </p>
       </div>
     </>
