9acb3eae9f69d4488681d68d21dfbe85b6176c10
Dimitar <wopz1337@gmail.com>
2022-11-12 12:45:44 +0200

    Comment Colors In Tailwind Config File

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,24 +5,24 @@ module.exports = {
     fill: (theme) => ({
       red: theme("colors.red.primary"),
     }),
-    colors: {
-      white: "#ffffff",
-      blue: {
-        medium: "#005c98",
-      },
-      black: {
-        light: "#262626",
-        faded: "#00000059",
-      },
-      gray: {
-        base: "#616161",
-        background: "#fafafa",
-        primary: "#dbdbdb",
-      },
-      red: {
-        primary: "#ed4956",
-      },
-    },
+    // colors: {
+    //   white: "#ffffff",
+    //   blue: {
+    //     medium: "#005c98",
+    //   },
+    //   black: {
+    //     light: "#262626",
+    //     faded: "#00000059",
+    //   },
+    //   gray: {
+    //     base: "#616161",
+    //     background: "#fafafa",
+    //     primary: "#dbdbdb",
+    //   },
+    //   red: {
+    //     primary: "#ed4956",
+    //   },
+    // },
   },
   plugins: [],
   variants: {
