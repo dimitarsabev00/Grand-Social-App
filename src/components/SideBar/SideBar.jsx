c680d93cd4480ea9477aa3447a590bef678ee196
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-02-06 15:01:58 +0200

    Added "© 2024 Built By dimitarsabev00" In SideBar Component

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -37,6 +37,17 @@ const SideBar = () => {
       ) : (
         <></>
       )}
+      <div className="text-xs text-black mt-4">
+        © 2024 Built By{" "}
+        <a
+          href="https://github.com/dimitarsabev00/"
+          target="_blank"
+          // style={{ color: "blue", fontSize: "14px" }}
+          className=" text-sm text-blue-600"
+        >
+          dimitarsabev00
+        </a>
+      </div>
     </div>
   );
 };
