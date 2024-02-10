19166c5ad05bb344b1e67d81629c52f530e8121a
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-02-10 14:41:48 +0200

    Added PrivateRoute & AuthRoute In App.jsx File

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,14 @@ import {
   Routes,
   Route,
   Navigate,
+  useLocation,
 } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import useAuthListener from "./hooks/useAuthListener";
 import ReactLoader from "./components/ReactLoader/loader";
 import { Toaster } from "react-hot-toast";
+import { useSelector } from "react-redux";
+import { selectUser } from "./app/features/userSlice";
 
 const Profile = lazy(() => import("./screens/Profile"));
 const Home = lazy(() => import("./screens/Home"));
@@ -18,6 +21,24 @@ const Login = lazy(() => import("./screens/Login"));
 const ErrorPage = lazy(() => import("./screens/ErrorPage"));
 function App() {
   const { user } = useAuthListener();
+
+  const currentUser = useSelector(selectUser);
+
+  const isAuthenticated = currentUser !== null;
+
+  const PrivateRoute = ({ children }) => {
+    let location = useLocation();
+    if (!isAuthenticated)
+      return <Navigate to="/login" state={{ from: location }} replace />;
+    return children;
+  };
+
+  const AuthRoute = ({ children }) => {
+    let location = useLocation();
+    if (isAuthenticated)
+      return <Navigate to={"/"} state={{ from: location }} replace />;
+    return children;
+  };
   return (
     <Router>
       <Suspense fallback={<ReactLoader />}>
@@ -25,17 +46,36 @@ function App() {
           <Route
             path="/"
             element={
-              localStorage.getItem("userAuth") ? (
+              <PrivateRoute>
                 <Home />
-              ) : (
-                <Navigate replace to="/login" />
-              )
+              </PrivateRoute>
             }
           />
 
-          <Route path="/login" element={<Login />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/profile/:username" element={<Profile />} />
+          <Route
+            path="/login"
+            element={
+              <AuthRoute>
+                <Login />
+              </AuthRoute>
+            }
+          />
+          <Route
+            path="/sign-up"
+            element={
+              <AuthRoute>
+                <SignUp />
+              </AuthRoute>
+            }
+          />
+          <Route
+            path="/profile/:username"
+            element={
+              <PrivateRoute>
+                <Profile />
+              </PrivateRoute>
+            }
+          />
           <Route path="/not-found" element={<ErrorPage />} />
         </Routes>
       </Suspense>
