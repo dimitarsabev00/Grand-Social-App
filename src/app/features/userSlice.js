e0505ad17777b1610491daac8dd6c9e8415ee62c
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-02-11 15:35:25 +0200

    Added Global Loading State, Added authUser & userProfile States, Added setAuthUser & setUserProfile Reducers

--- src/app/features/userSlice.js
+++ src/app/features/userSlice.js
@@ -3,19 +3,37 @@ import { createSlice } from "@reduxjs/toolkit";
 export const userSlice = createSlice({
   name: "user",
   initialState: {
-    user: JSON.parse(localStorage.getItem("user-info")) || null,
+    authUser: JSON.parse(localStorage.getItem("user-info")) || null,
+    userProfile: null,
+    isLoading: false,
   },
   reducers: {
+    startLoading: (state) => ({ ...state, isLoading: true }),
+    stopLoading: (state) => ({ ...state, isLoading: false }),
     login: (state, action) => {
-      state.user = action.payload;
+      state.authUser = action.payload;
     },
     logout: (state) => {
-      state.user = null;
+      state.authUser = null;
+    },
+    setAuthUser: (state, action) => {
+      state.authUser = action.payload;
+    },
+    setUserProfile: (state, action) => {
+      state.userProfile = action.payload;
     },
   },
 });
 
-export const { login, logout } = userSlice.actions;
-export const selectUser = (state) => state.user.user;
+export const {
+  login,
+  logout,
+  setUserProfile,
+  setAuthUser,
+  startLoading,
+  stopLoading,
+} = userSlice.actions;
+export const selectUser = (state) => state.user.authUser;
+export const selectUserProfile = (state) => state.user.userProfile;
 
 export default userSlice.reducer;
