b5eb980fc2bb3d7c1062ebabc706b693ed7cf605
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-02-12 14:40:09 +0200

    Refactor Profile Screen

--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -1,33 +1,76 @@
-import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import { getUserByUsername } from "../services/firebase";
+import { useParams } from "react-router-dom";
 import Header from "../components/Header/Header";
-import UserProfile from "../components/Profile/UserProfile";
+import useGetUserProfileByUsername from "../hooks/useGetUserProfileByUsername";
+import HeaderUserProfile from "../components/Profile/HeaderUserProfile";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import PostsUserProfile from "../components/Profile/PostsUserProfile";
+import useGetUserPosts from "../hooks/useGetUserPosts";
 
 const Profile = () => {
   const { username } = useParams();
-  const [user, setUser] = useState(null);
-  const navigate = useNavigate();
-  useEffect(() => {
-    const checkUserExist = async () => {
-      const [user] = await getUserByUsername(username);
-      if (user?.userId) {
-        setUser(user);
-      } else {
-        navigate("/not-found");
-      }
-    };
-    checkUserExist();
-  }, [username, navigate]);
+  const { isLoading, userProfile } = useGetUserProfileByUsername(username);
+  const { isLoadingPosts, posts } = useGetUserPosts();
 
-  return user?.username ? (
-    <div>
+  const userNotFound = !isLoading && !userProfile;
+  const postsNotFound = posts.length === 0;
+
+  return (
+    <>
       <Header />
       <div className="mx-auto max-w-screen-lg">
-        <UserProfile user={user} />
+        {!isLoading && userProfile && (
+          <HeaderUserProfile userProfile={userProfile} />
+        )}
+        {userNotFound && (
+          <p className="text-center text-2x1">Not Found User!</p>
+        )}
+
+        {!isLoadingPosts && posts && <PostsUserProfile posts={posts} />}
+        {postsNotFound && <p className="text-center text-2x1">No Posts Yet!</p>}
       </div>
-    </div>
-  ) : null;
+
+      {isLoading && (
+        <div className="mx-auto max-w-screen-lg">
+          {/* Skeleton For HeaderUserProfile */}
+          <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
+            <div className="container flex justify-center items-center">
+              <Skeleton circle height={150} width={150} count={1} />
+            </div>
+            <div className="flex items-center justify-center flex-col col-span-2">
+              <div className="container flex mt-4">
+                <Skeleton count={1} width={200} height={24} />
+              </div>
+              <div className="container mt-4">
+                <p className="font-medium">
+                  <Skeleton count={1} height={24} width={300} />
+                </p>
+              </div>
+              <div className="container mt-4">
+                <Skeleton count={1} height={24} width={200} />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {isLoadingPosts && (
+        <div className="mx-auto max-w-screen-lg">
+          {/* Skeleton For PostsUserProfile */}
+          <div className="h-16 border-t border-gray-500 mt-12 pt-4">
+            <div className="grid grid-cols-3 gap-4 mt-4 mb-12">
+              <Skeleton key={12} width={330} height={165} />
+              <Skeleton key={12} width={330} height={165} />
+              <Skeleton key={12} width={330} height={165} />
+              <Skeleton key={12} width={330} height={165} />
+              <Skeleton key={12} width={330} height={165} />
+              <Skeleton key={12} width={330} height={165} />
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Profile;
