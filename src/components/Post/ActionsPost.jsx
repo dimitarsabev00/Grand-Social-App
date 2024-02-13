3f0a2489942094f6f5511d6486e6535f8ea4d96d
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-02-13 12:21:30 +0200

    Refactor Like Post Functionality

--- src/components/Post/ActionsPost.jsx
+++ src/components/Post/ActionsPost.jsx
@@ -1,46 +1,25 @@
-import { arrayRemove, arrayUnion, doc, updateDoc } from "firebase/firestore";
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
-import { selectUser } from "../../app/features/userSlice";
-import { db } from "../../configs/firebase";
-
-const ActionsPost = ({ docId, totalLikes, likedPhoto, handleFocus }) => {
-  const user = useSelector(selectUser);
-  const [toggleLiked, setToggleLiked] = useState(likedPhoto);
-  const [likes, setLikes] = useState(totalLikes);
-  const handleToggleLiked = async () => {
-    setToggleLiked((toggleLiked) => !toggleLiked);
-
-    const userDoc = doc(db, "posts", docId);
-    const newField = {
-      likes: toggleLiked ? arrayRemove(user?.uid) : arrayUnion(user?.uid),
-      userLikedPhoto: !toggleLiked,
-    };
-    await updateDoc(userDoc, newField);
-    setLikes((likes) => (toggleLiked ? likes - 1 : likes + 1));
-  };
+import React from "react";
+import useLikePost from "../../hooks/useLikePost";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+const ActionsPost = ({ handleFocus, post }) => {
+  const { isLiked, likes, handleLikePost } = useLikePost(post);
   return (
     <>
       <div className="flex justify-between p-4">
         <div className="flex">
-          <svg
-            onClick={handleToggleLiked}
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-6 h-6"
-            className={`w-6 mr-4 select-none cursor-pointer ${
-              toggleLiked ? "fill-red text-red-primary" : "text-black-light"
-            }`}
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+          {isLiked ? (
+            <FavoriteIcon
+              className="text-red-500 cursor-pointer mr-4"
+              onClick={handleLikePost}
             />
-          </svg>
+          ) : (
+            <FavoriteBorderIcon
+              onClick={handleLikePost}
+              className="cursor-pointer mr-4"
+            />
+          )}
+
           <svg
             onClick={handleFocus}
             xmlns="http://www.w3.org/2000/svg"
