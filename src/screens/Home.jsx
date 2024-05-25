5e81e13f01b2df453710916cca8615739e00ed64
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-05-25 12:35:25 +0300

    Added Responsive Design For Home Screen

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,21 +1,22 @@
-import { useEffect } from "react";
-import Feed from "../components/Feed/Feed";
-import Header from "../components/Header/Header";
-import SideBar from "../components/SideBar/SideBar";
+import { useEffect } from "react"
+import Feed from "../components/Feed/Feed"
+import Header from "../components/Header/Header"
+import SideBar from "../components/SideBar/SideBar"
+
 const Home = () => {
   useEffect(() => {
-    document.title = "Grand Social";
-  }, []);
+    document.title = "Grand Social"
+  }, [])
 
   return (
     <div className="bg-gray-background">
       <Header />
-      <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg ">
-        <Feed />
-        <SideBar />
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
+        <Feed className="col-span-2 md:col-span-2" />
+        <SideBar className="col-span-1 md:col-span-1" />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
