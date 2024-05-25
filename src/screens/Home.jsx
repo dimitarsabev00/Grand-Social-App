8f6dbff4ae90e7062719298b5f4456b4e0288849
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-05-25 12:07:54 +0300

    Formating Code In Home Screen

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -6,13 +6,12 @@ const Home = () => {
   useEffect(() => {
     document.title = "Grand Social";
   }, []);
+
   return (
     <div className="bg-gray-background">
       <Header />
-
       <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg ">
         <Feed />
-
         <SideBar />
       </div>
     </div>
