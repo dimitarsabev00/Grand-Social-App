3dade7df4ec30457c1bc13ec556c6492e30051a5
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-05-25 12:38:24 +0300

    Fix Bug With Render Image In ImagePost Component In Post Component

--- src/components/Post/ImagePost.jsx
+++ src/components/Post/ImagePost.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ImagePost = ({ postImage }) => {
-  return <img src={postImage} alt="Post Image" />;
+  return <img src={postImage} alt="Post Image" className="w-full" />;
 };
 
 export default ImagePost;
